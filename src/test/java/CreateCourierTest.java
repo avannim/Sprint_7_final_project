2a257a446f2286e8afcec041a176df8e645cf2e7
Anastasia  Fedorenko <avannim88@yandex.ru>
2026-02-09 20:07:05 +0300

    классы тестирования ручек

---
+++ src/test/java/CreateCourierTest.java
@@ -0,0 +1,14 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateCourierTest {
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
+    }
+
+
+}
---
+++ src/test/java/GetOrderByNumberTest.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+
+import static io.restassured.RestAssured.given;
+
+public class GetOrderByNumberTest {
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
+    }
+}
